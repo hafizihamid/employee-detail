e52a2edf7c27cf2998ca5d509bb29706c280bab0
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-01-09 23:22:47 +0800

    Add employee seeder

---
+++ backend/database/seeders/EmployeeSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        foreach (range(1,10) as $index) {
+            DB::table("employees")->insert([
+            "first_name" => $faker->name(),
+            "last_name" => $faker->name(),
+            "email" => $faker->safeEmail,
+            "phone_no" => $faker->numerify('+601#########'),
+            "join_date" =>  $faker->date('Y_m_d'),
+            "job_title" => $faker->randomElement(["developer manager", "general manager"]),
+            "manager_id" => $faker->numberBetween(25, 50),
+            "updated_at" => $faker->dateTimeBetween('-1 week', '+1 week'),
+            "created_at" => $faker->dateTimeBetween('-1 week', '+1 week'),
+        ]);
+
+        }
+    }
+}
+
