36c5d6c45b71f4ea1d4d66dc83ccfe325d2997a1
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-01-10 05:20:23 +0800

    Update environment url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const apiUrl = `http://127.0.0.1:8000`;
+
 export const environment = {
-  production: false
+  apiUrl: `${apiUrl}/api`,
+  production: false,
 };
 
 /*
