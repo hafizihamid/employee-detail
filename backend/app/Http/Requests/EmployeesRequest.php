bf37ebe803bf39c18158fbb69709345d5f292a00
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-01-09 23:26:33 +0800

    Add request

---
+++ backend/app/Http/Requests/EmployeesRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeesRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $routeName = $this->route()->getName();
+
+        $rules = [
+            'id' => 'numeric|exists:employees,id',
+        ];
+
+        if ($routeName == 'create' || $routeName == 'update') {
+            $rules['first_name'] = 'required|regex:/^[a-zA-Z0-9]+$/';
+            $rules['last_name'] = 'required|regex:/^[a-zA-Z0-9]+$/';
+            $rules['email'] = 'required|email|unique:employees';
+            $rules['phone_no'] = 'required';
+            $rules['join_date'] = 'required|date_format:Y-m-d';
+            $rules['job_title'] = 'required';
+            $rules['manager_id'] = 'required';
+        }
+        return $rules;
+    }
+
+    public function all($keys = null)
+    {
+        $data = parent::all();
+        if ($this->route('id')) {
+            $data['id'] = $this->route('id');
+        }
+
+        return $data;
+    }
+}
